efb4be5c3bfcf7cd44f07ae9d80479f412490947
berkay oren <benberkayoren@gmail.com>
2022-08-26 21:26:41 +0300

    Js teamwork

---
+++ TeamWork/TeamWorkJs/script.js
@@ -0,0 +1,25 @@
+let word = "Clarusway Rocks!";
+console.log(`Orijinal Hali => ${word}`);
+
+let word_split = word.split("").reverse().join("");
+
+console.log(word_split);
+
+//! split heceleme keyword'u reverse tserçevirme keyword'u join ise birleştime
+
+let items = "Welcome to Clarusway! Enjoy yourself...";
+
+items.reverse;
+console.log(items.split(" ").reverse().join(" "));
+
+let fruit = ["apple", "peach", "cherry"];
+
+fruit[fruit.length] = "orange";
+
+console.log(fruit);
+
+fruit.push("banana");
+
+//! unshift basina ekliyor - push direkt sonuna ekliyor. */
+
+console.log(fruit);
