00883a3aa2ce882b3f9f3df11634f0f0b1d8678e
berkay oren <benberkayoren@gmail.com>
2022-08-29 20:34:03 +0300

    Arrays

--- JavaScript/Js-Arrays/arrays.js
+++ JavaScript/Js-Arrays/arrays.js
@@ -2,3 +2,24 @@
 //?              ARRAYS
 
 console.log("******arrays*****");
+
+//* Dizi Tanimlama
+//* ----------------------------------
+
+//! 1. Yöntem (Array Literal) - Tercih edilen yöntem
+
+const isimler = ["ahmet", "ismet", "can", "canan"];
+console.log(isimler);
+console.log(typeof isimler);
+console.log(isimler.length);
+
+//! 2. Yöntem (object constructor)
+const diller = new Array("C++", "JavaScript", "Assembly", "Go", "Ruby");
+console.log(diller);
+
+const numbers = new Array(3, 2, 1);
+console.log(numbers);
+
+//! 10 elemanlık boş bir array oluşturdu.
+const numbers1 = new Array(10);
+console.log(numbers1);
