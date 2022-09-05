4d87870589770d7fb746f8a8b470bf6db21cdf23
berkay oren <benberkayoren@gmail.com>
2022-09-05 21:05:04 +0300

    1st commit

---
+++ JavaScript/Js-Objects/object.js
@@ -0,0 +1,51 @@
+//* ===================================================
+//*                 (OBJECTS)
+//* ===================================================
+
+//? Diziler sirali bellek bölgeleridir.Bu yuzden dizilere
+//? indeksleme ile erisebiliriz. Ancak daha karmasik (unstructured)
+//? verilerimiz icin  Object'leri kullanabiliriz.
+
+//? Object'lerde Key-value (property-value) yapisi kullanilir.
+//? Herhangi bir veriyi erismek icin property (key) adi kullanilir.
+
+console.log("******* Objects *********");
+
+//! 3 farkli yontem ile Object olusturulabilir
+//* ---------------------------------------------------------
+//* 1- Object() class'ından new Operatörü ile
+//* ---------------------------------------------------------
+
+const arabalar = new Object();
+arabalar.marka = "BMW";
+arabalar.motor = "1.3";
+arabalar.model = 2022;
+arabalar.lpg = true;
+console.log(arabalar);
+
+//!Read
+console.log(arabalar.lpg); //? .notation
+console.log("MODEL:", arabalar["model"]); //? Square bracket notation
+
+//? Square bracket yonteminin en buyuk avantaji key degerini
+//? degisken olarak kullanabilmemizdir.
+const key = "marka";
+console.log(arabalar[key]);
+
+//* ---------------------------------------------------------
+//* 2- object constructor'i kullanarak (OOP ile ayrintilandirilacak)
+//* ---------------------------------------------------------
+
+//? Object Constructure
+function Personal(id, ad, maas) {
+  this.perId = id;
+  this.perAd = ad;
+  this.maas = maas;
+  //   console.log(this);
+}
+const kisi1 = new Personal("1234567890", "Mustafa", 15000);
+const kisi2 = new Personal("1778977412", "Canan", 20000);
+// console.log(kisi1);
+console.log(kisi1.ad);
+console.log(kisi2.maas);
+console.log(kisi2.["id"]);
