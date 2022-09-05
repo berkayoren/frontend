2d2f852606e6744af4378ebf19bb73aa2c622186
berkay oren <benberkayoren@gmail.com>
2022-09-05 22:22:44 +0300

    commit

--- JavaScript/Js-Objects/object.js
+++ JavaScript/Js-Objects/object.js
@@ -48,4 +48,68 @@ const kisi2 = new Personal("1778977412", "Canan", 20000);
 // console.log(kisi1);
 console.log(kisi1.ad);
 console.log(kisi2.maas);
-console.log(kisi2.["id"]);
+console.log(kisi2["id"]);
+
+//*------------------------------------------
+//* 3- Object literal (en çok tercih edilen yöntem)
+//*------------------------------------------
+
+const worker = {
+  name: "Can",
+  surname: "Canan",
+  age: 33,
+  job: "developer",
+  languages: ["C++", "Java", "JavaScript", "Python", "Go"],
+  salary: 140000,
+};
+console.log(worker);
+
+console.log("JOB:", worker.job);
+console.log("Lang", worker["languages"]);
+console.log(worker.languages[2]);
+
+worker["languages"].forEach((l) => console.log(1));
+
+worker.dob = "1990";
+worker.email = "can@gmail.com";
+console.log(worker);
+
+worker["salary"] = worker["salary"] * 1.1;
+console.log(worker);
+
+//* Object Copy
+
+const person = worker;
+console.log("PERSON:", person);
+
+person.dob = "2000";
+console.log("PERSON:", person);
+
+//! Object.create(), Object.assign(), spread (...), concat(), slice() -> Shallow.Copying
+
+// person dob = 2000
+// console.log("PERSON:", deepCoptOfWorker);
+
+//*========================================
+//* Object Metotlari
+//*========================================
+
+const personal = {
+  name: "Can",
+  surname: "Canan",
+  age: 33,
+  dob: "1990",
+  job: "developer",
+  salary: "140000",
+  drivingLicense: true,
+  calculateAge: function name() {
+    return new Date().getFullYear() - this.dob;
+  },
+  summary: () => {
+    console.log(this); //? window
+    return `${this.name} is ${this.calculateAge()} years old`;
+  },
+};
+
+console.log("Age:", personal.calculateAge());
+console.log("SUMMARY:", personal.summary());
