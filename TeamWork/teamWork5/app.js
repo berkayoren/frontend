502c01c940ace2e7b087d80029e5db5160d86414
berkay oren <benberkayoren@gmail.com>
2022-09-10 17:39:19 +0300

    end of teamwork assingment added

--- TeamWork/teamWork5/app.js
+++ TeamWork/teamWork5/app.js
@@ -25,3 +25,46 @@ const roman = (num) => {
   return str;
 };
 console.log(roman(2022));
+
+// Girilen kelimenin ortasindaki harfi bulan bir fonksiyon yazin.Ama kelimenin uzunlugu tek ise ortadaki harfi cift ise oratdaki iki harfi döndürün
+// Beispiel => test    === es
+//             testing === t
+//             middle  === dd
+//             A       === a
+
+const middle = (yen) => {
+  let yeni = yen.split("");
+  //   if (yeni.length % 2) {
+  //     console.log(yeni[Math.floor(yeni.length / 2)]);
+  //   } else {
+  //     console.log(
+  //       yeni[Math.floor(yeni.length / 2 - 1)] + yeni[Math.floor(yeni.length / 2)]
+  //     );
+  //   }
+  return yeni.length % 2 === 0
+    ? yeni[Math.floor(yeni.length / 2 - 1)] + yeni[Math.floor(yeni.length / 2)]
+    : yeni[Math.floor(yeni.length / 2)];
+};
+
+console.log(middle("test"));
+
+/* İzogram tekrar eden harfleri olmayan bir kelimedir.
+Bir kelimenin izogram olup olmadığını bulan bir fonksiyon yazin
+
+Beispiel => "terminal" --> true
+            "helfen" --> false
+            "Ausdrucken" --> false (ignore letter case)
+*/
+const arr = "ehelfen".toLowerCase().split("");
+
+const filt = arr.filter((e, i) => arr.indexOf(e) === i);
+console.log(filt);
+if (arr.length === filt.length) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+/* girilen sayilari kendi degerlerine göre degilde basamak toplamlarinin degerine göre sort eden bir program yazi
+Beispeil => '56 65 74 100 99 68 86 180 90' ---> '100', '180', '90', '56', '65', '74', '68', '86', '99'
+*/
