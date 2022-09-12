fcaefa4ebf53b0fc1423fd9db50a9443189059e3
berkay oren <benberkayoren@gmail.com>
2022-09-12 22:45:40 +0300

    Commit

--- JavaScript/Js Events/event-example/app.js
+++ JavaScript/Js Events/event-example/app.js
@@ -1,1 +1,33 @@
 console.log("**** app.js *******");
+
+//? Selectors
+const langInput = document.querySelector(".lang-input");
+const addBtn = document.querySelector("#add-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const langList = document.getElementById("lang-list");
+
+const newUl = document.createElement("ul");
+langList.appendChild(newUl);
+
+addBtn.addEventListener("click", () => {
+  if (!langInput.value) {
+    alert("Please Enter a Language");
+  } else {
+    newUl.innerHTML += `<li>${langInput.value}</li>`;
+    langInput.value = "";
+  }
+  langInput.focus();
+});
+
+//? deleteBtn event handler
+deleteBtn.addEventListener("click", () => {
+  newUl.childElementCount > 0
+    ? newUl.removeChild(newUl.lastElementChild)
+    : alert("There is no item to delete");
+  newUl.removeChild(newUl.lastElementChild);
+});
+
+//? onload event handler
+window.addEventListener("load", () => {
+  langInput.focus();
+});
