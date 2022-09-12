7c729d8945b6e228d9be62d2a99762b8f1821b7a
berkay oren <benberkayoren@gmail.com>
2022-09-12 20:48:58 +0300

    commit

--- JavaScript/Js Events/events-intro/events.js
+++ JavaScript/Js Events/events-intro/events.js
@@ -12,12 +12,17 @@ const h1 = document.querySelector("#header h1");
 h1.onmouseover = function () {
   h1.style.color = "red";
   h1.style.fontWeight = "300";
+  h1.style.cursor = "pointer";
 };
 
 h1.onmouseout = () => {
   h1.style.color = "black";
   h1.style.fontWeight = "900";
 };
+
+h1.addEventListener("click", () => {
+  alert("H1 Pressed");
+});
 
 //* EXAMPLE-2 (addEventListener())
 //* -------------------------------------------------
