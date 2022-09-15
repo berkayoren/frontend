e27a29ebca26c3df8693c9722540014d2c433055
berkay oren <benberkayoren@gmail.com>
2022-09-15 23:17:25 +0300

    fixed and end of the lesson

--- HTML-CSS/17_animation/4-game/game.js
+++ HTML-CSS/17_animation/4-game/game.js
@@ -1,6 +1,8 @@
 const body = document.querySelector("body");
 const car = document.querySelector(".car");
 const ground = document.querySelector(".ground");
+const carImg = document.querySelector(".car img");
+let flag = true;
 
 body.addEventListener("keydown", (e) => {
   if (e.code === "Enter") {
@@ -10,6 +12,18 @@ body.addEventListener("keydown", (e) => {
     } else {
       ground.classList.remove("right");
       car.classList.remove("pop-up");
+    }
+  }
+});
+
+body.addEventListener("keydown", (e) => {
+  if (e.code === "Escape") {
+    if (flag) {
+      carImg.setAttribute("src", "img/Img_06.png");
+      flag = false;
+    } else {
+      carImg.setAttribute("src", "img/Img_05.png");
+      flag = true;
     }
   }
 });
