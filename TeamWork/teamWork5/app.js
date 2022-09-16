d344e9fb2ff19bef6feb81706468e3842d952d99
berkay oren <benberkayoren@gmail.com>
2022-09-16 22:48:29 +0300

    array sort

--- TeamWork/teamWork5/app.js
+++ TeamWork/teamWork5/app.js
@@ -1,30 +1,30 @@
-const roman = (num) => {
-  const romanNummer = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1,
-  };
-  let str = "";
-  let count;
-  for (i in romanNummer) {
-    count = Math.floor(num / romanNummer[i]);
-    num %= romanNummer[i];
+// const roman = (num) => {
+//   const romanNummer = {
+//     M: 1000,
+//     CM: 900,
+//     D: 500,
+//     CD: 400,
+//     C: 100,
+//     XC: 90,
+//     L: 50,
+//     XL: 40,
+//     X: 10,
+//     IX: 9,
+//     V: 5,
+//     IV: 4,
+//     I: 1,
+//   };
+//   let str = "";
+//   let count;
+//   for (i in romanNummer) {
+//     count = Math.floor(num / romanNummer[i]);
+//     num %= romanNummer[i];
 
-    str += i.repeat(count);
-  }
-  return str;
-};
-console.log(roman(2022));
+//     str += i.repeat(count);
+//   }
+//   return str;
+// };
+// console.log(roman(2022));
 
 // Girilen kelimenin ortasindaki harfi bulan bir fonksiyon yazin.Ama kelimenin uzunlugu tek ise ortadaki harfi cift ise oratdaki iki harfi döndürün
 // Beispiel => test    === es
@@ -32,8 +32,8 @@ console.log(roman(2022));
 //             middle  === dd
 //             A       === a
 
-const middle = (yen) => {
-  let yeni = yen.split("");
+// const middle = (yen) => {
+//   let yeni = yen.split("");
   //   if (yeni.length % 2) {
   //     console.log(yeni[Math.floor(yeni.length / 2)]);
   //   } else {
@@ -41,12 +41,12 @@ const middle = (yen) => {
   //       yeni[Math.floor(yeni.length / 2 - 1)] + yeni[Math.floor(yeni.length / 2)]
   //     );
   //   }
-  return yeni.length % 2 === 0
-    ? yeni[Math.floor(yeni.length / 2 - 1)] + yeni[Math.floor(yeni.length / 2)]
-    : yeni[Math.floor(yeni.length / 2)];
-};
+//   return yeni.length % 2 === 0
+//     ? yeni[Math.floor(yeni.length / 2 - 1)] + yeni[Math.floor(yeni.length / 2)]
+//     : yeni[Math.floor(yeni.length / 2)];
+// };
 
-console.log(middle("test"));
+// console.log(middle("test"));
 
 /* İzogram tekrar eden harfleri olmayan bir kelimedir.
 Bir kelimenin izogram olup olmadığını bulan bir fonksiyon yazin
@@ -55,16 +55,25 @@ Beispiel => "terminal" --> true
             "helfen" --> false
             "Ausdrucken" --> false (ignore letter case)
 */
-const arr = "ehelfen".toLowerCase().split("");
+// const arr = "ehelfen".toLowerCase().split("");
 
-const filt = arr.filter((e, i) => arr.indexOf(e) === i);
-console.log(filt);
-if (arr.length === filt.length) {
-  console.log("true");
-} else {
-  console.log("false");
+// const filt = arr.filter((e, i) => arr.indexOf(e) === i);
+// console.log(filt);
+// if (arr.length === filt.length) {
+//   console.log("true");
+// } else {
+//   console.log("false");
 }
 
 /* girilen sayilari kendi degerlerine göre degilde basamak toplamlarinin degerine göre sort eden bir program yazi
 Beispeil => '56 65 74 100 99 68 86 180 90' ---> '100', '180', '90', '56', '65', '74', '68', '86', '99'
 */
+
+const sayi= "56 65 74 100 99 68 86 180 90".split()
+
+const arr= sayi.map((e)=>{
+  return{
+    key:e,
+    val:
+  }
+})
