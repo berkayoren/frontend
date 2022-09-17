39275faa3d4d851a6e5ffccc9b98497b6c38640c
berkay oren <benberkayoren@gmail.com>
2022-09-17 22:54:54 +0300

    changed option prompt

--- TeamWork/checkout/app.js
+++ TeamWork/checkout/app.js
@@ -14,7 +14,7 @@ document.querySelector(".products").onclick = (e) => {
         e.target.parentElement.parentElement.parentElement
       );
     } else {
-      if (confirm("Produkt wird gelöscht?")) {
+      if (confirm("Product will be deleted")) {
         e.target.parentElement.parentElement.parentElement.parentElement.remove();
         cartTotal();
       }
