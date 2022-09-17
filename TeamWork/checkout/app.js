569933e26493c8f79c7acb1de2bdf33d4320c708
berkay oren <benberkayoren@gmail.com>
2022-09-17 16:13:53 +0300

    will be fixed

--- TeamWork/checkout/app.js
+++ TeamWork/checkout/app.js
@@ -1,9 +1,32 @@
 document.querySelector(".products").onclick = (e) => {
   if (e.target.className === "fas fa-minus") {
+    if (e.target.parentElement.nextElementSibling.innerText > 1) {
+      e.target.parentElement.nextElementSibling.innerText--;
+      productTotal(
+        e.target.parentElement.nextElementSibling.innerText,
+        e.target.parentElement.parentElement.parentElement
+      );
+    } else {
+      if (confirm("Are you sure to delete this product")) {
+        e.target.parentElement.parentElement.parentElement.parentElement.remove();
+        cartTotal();
+      }
+    }
     console.log(e.target.parentElement.nextElementSibling.innerText);
   } else if (e.target.className === "fas fa-plus") {
-    console.log("plusa tıklandi");
+    e.target.parentElement.previousElementSibling.innerText++;
+    productTotal();
   } else if (e.target.className === "removeProduct") {
-    console.log("remove tiklandi");
+    e.target.parentElement.parentElement.parentElement.remove();
+    cartTotal();
   }
 };
+
+const productTotal = (e, b) => {
+  let productPrice = b.children[1].children[0].firstChild.innextext;
+  let productTotal = b.children[4];
+  let amount = e;
+  productTotal.innerText = console.log(productPrice);
+  cartTotal();
+};
+const cartTotal = () => {};
