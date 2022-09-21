1fa0cb9cb678fedfa4ac4d423c3f03648195a79b
berkay oren <benberkayoren@gmail.com>
2022-09-21 21:24:30 +0300

    functions

--- JavaScript/checkoutPageInclass/checkout.js
+++ JavaScript/checkoutPageInclass/checkout.js
@@ -1,17 +1,16 @@
-const taxRate = 0.18;
-const shippingPrice = 15;
-const shippingFreePrice = 300;
+// const taxRate = 0.18;
+// const shippingPrice = 15;
+// const shippingFreePrice = 300;
 
 window.addEventListener("load", () => {
-  // set iterms to LocalStorage
-  localStorage.setItem("taxRate", taxRate);
-  localStorage.setItem("shippingPrice", shippingPrice);
-  localStorage.setItem("shippingFreePrice", shippingFreePrice);
-
-  // set iterms to sessionStorage
-  localStorage.setItem("taxRate", taxRate);
-  localStorage.setItem("shippingPrice", shippingPrice);
-  localStorage.setItem("shippingFreePrice", shippingFreePrice);
+  //   // set iterms to LocalStorage
+  //   localStorage.setItem("taxRate", taxRate);
+  //   localStorage.setItem("shippingPrice", shippingPrice);
+  //   localStorage.setItem("shippingFreePrice", shippingFreePrice);
+  //   // set iterms to sessionStorage
+  //   localStorage.setItem("taxRate", taxRate);
+  //   localStorage.setItem("shippingPrice", shippingPrice);
+  //   localStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
 
 const productsDiv = document.querySelector(".products");
@@ -21,19 +20,30 @@ productsDiv.addEventListener("click", (event) => {
     // console.log("minus btn is clicked");
     if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
       event.target.parentElement.querySelector(".quantity").innerText--;
+      calculateProductPrice();
+      calculateCartPrice();
     } else {
       if (confirm("Do you want to remove the product?")) {
         //* remove
         event.target.parentElement.parentElement.parentElement.remove();
+        calculateCartPrice();
       }
     }
     event.target.parentElement.querySelector(".quantity").innerText--;
   } else if (event.target.classList.contains("fa-plus")) {
-    event.target.previousElementSibling.innerText++;
     // console.log("plus btn is clicked");
+    event.target.previousElementSibling.innerText++;
+    calculateProductPrice();
+    calculateCartPrice();
   } else if (event.target.className == "remove-product") {
+    event.target.parentElement.parentElement.parentElement.remove();
+    calculateCartPrice();
     // console.log("remove btn is clicked");
   } else {
     // console.log("other element is clicked");
   }
 });
+
+const calculateProductPrice = () => {};
+
+const calculateCartPrice = () => {};
