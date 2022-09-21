62146928d78fd78d03277395b27c085c4a4ceaad
berkay oren <benberkayoren@gmail.com>
2022-09-21 20:57:53 +0300

    Commit

--- JavaScript/checkoutPageInclass/checkout.js
+++ JavaScript/checkoutPageInclass/checkout.js
@@ -13,3 +13,27 @@ window.addEventListener("load", () => {
   localStorage.setItem("shippingPrice", shippingPrice);
   localStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
+
+const productsDiv = document.querySelector(".products");
+//* Capturing vs. Bubbling
+productsDiv.addEventListener("click", (event) => {
+  if (event.target.className == "fa-solid fa-minus") {
+    // console.log("minus btn is clicked");
+    if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
+      event.target.parentElement.querySelector(".quantity").innerText--;
+    } else {
+      if (confirm("Do you want to remove the product?")) {
+        //* remove
+        event.target.parentElement.parentElement.parentElement.remove();
+      }
+    }
+    event.target.parentElement.querySelector(".quantity").innerText--;
+  } else if (event.target.classList.contains("fa-plus")) {
+    event.target.previousElementSibling.innerText++;
+    // console.log("plus btn is clicked");
+  } else if (event.target.className == "remove-product") {
+    // console.log("remove btn is clicked");
+  } else {
+    // console.log("other element is clicked");
+  }
+});
