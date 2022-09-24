ac5dcd753cd823a53bd14bf142f466a3c1e4baf3
berkay oren <benberkayoren@gmail.com>
2022-09-24 16:55:23 +0300

    Finished.

--- TeamWork/weatherApp/app.js
+++ TeamWork/weatherApp/app.js
@@ -18,9 +18,37 @@ const getWeatherData = async () => {
   let weatherType = "metric";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=${weatherType}`;
 
-  const res = await fetch(url);
-  const data = await res.json();
-  console.log(res);
-  const { main, name, sys, weather } = data;
-  const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@4x.png`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(res);
+    const { main, name, sys, weather } = data;
+    const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@4x.png`;
+    cityCart.innerHTML = `<div class="head">
+                        <h2 class="cityName"><span>${name}</span><sup>${
+      sys.country
+    }</sup>
+                        </h2>
+                        <div class="closeIcon">X</div>
+                    </div>
+
+                    <div class="cityTemp">${Math.round(
+                      main.temp
+                    )}<sup>°C</sup></div>
+
+                    <img class="cityIcon" src="${iconUrl}">
+                    <p>${weather[0].description}</p>`;
+    resultArea.appendChild(cityCart);
+    warning.innerText = "";
+    form.reset();
+  } catch (error) {
+    warning.innerText = `The City you have searched can not be found.`;
+    form.reset();
+  }
+};
+
+resultArea.onclick = (e) => {
+  if (e.target.className === "closeIcon") {
+    e.target.closest(".city").remove();
+  }
 };
