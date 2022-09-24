de5d5c71921bb7fc9e1fe2b63784023726f42bb9
berkay oren <benberkayoren@gmail.com>
2022-09-24 12:50:30 +0300

    commit

--- JavaScript/Js-Async2/async-await/async-await.js
+++ JavaScript/Js-Async2/async-await/async-await.js
@@ -20,12 +20,38 @@
 
 const getNews = async function () {
   const API_KEY = "6ebd9171d31847dbb94ded4064b9ab78";
-
   const url =
-    "https://newsapi.org/v2/top-headlines?country=tr&apiKey=" + API_KEY;
-
-  const res = await fetch(url);
-  console.log(res);
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY;
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Something went wrong ${res.status}`);
+    }
+    const data = await res.json();
+    console.log(data.articles);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-getNews();
+const renderNews = (news) => {
+  const newsList = document.getElementById("news-list");
+
+  news.forEach((item) => {
+    const { title, description, urlToImage, url } = item; //! dest
+    newsList.innerHTML += `
+    <div class="col-md6 col-lg-4 col-xl-3">
+    <div class="card" style="width: 18rem;">
+    <img src="${urlToImage}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="${url}" target="_blank class="btn btn-primary">Details</a>
+        </div>
+    </div>
+    </div>
+    `;
+  });
+};
+
+window.addEventListener("load", getNews);
