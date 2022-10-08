afc7f126fd43597cf1efe7b3bbd3856d1acb8c05
berkay oren <benberkayoren@gmail.com>
2022-10-08 11:56:32 +0300

    react

--- React/react2jsx_comp_basicStyling/rc02_jsx_comp_basicStyling/src/index.js
+++ React/react2jsx_comp_basicStyling/rc02_jsx_comp_basicStyling/src/index.js
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// import App from "./App";
+import App from "./App";
 import "./index.css";
 
-const element = () => {
-  return (
-    <>
-      <h1>Hello React</h1>
-      <p>Merhaba</p>
-    </>
-  );
-};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element());
+root.render(<App />);
