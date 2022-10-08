12dd8a36108fb6862bb6e35ae7a5b8a0283b02b1
berkay oren <benberkayoren@gmail.com>
2022-10-08 13:42:23 +0300

    person

---
+++ React/rc02_props/src/components/Person.jsx
@@ -0,0 +1,14 @@
+const Person = () => {
+  return (
+    <div>
+      <p>Hello Harwey</p>
+      <img
+        src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg"
+        alt="img1"
+      />
+      <p>555555555</p>
+    </div>
+  );
+};
+
+export default Person;
