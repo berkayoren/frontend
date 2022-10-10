f217cbbe79da79c0100b5b807b170ee9b963109d
berkay oren <benberkayoren@gmail.com>
2022-10-10 21:56:05 +0300

    mdule

--- React/rc03_module-css/src/components/card/Card.jsx
+++ React/rc03_module-css/src/components/card/Card.jsx
@@ -1,12 +1,13 @@
 import Buton from "../buton/Buton";
-import "./Card.css";
+// import "./Card.css";
+import CardStyle from "./card.module.css";
 
 const Card = ({ language, btn, img }) => {
   //   const { language, btn, img } = props;
   return (
     <div>
-      <h1>{language} </h1>
-      <img src={img} alt="img" />
+      <h1 className={CardStyle["title"]}>{language} </h1>
+      <img className={CardStyle["images"]} src={img} alt="img" />
       <Buton btn={btn} />
     </div>
   );
