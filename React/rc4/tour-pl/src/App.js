ba8f711910278c3a9e2c86e8b4c4ee54722d80e7
berkay oren <benberkayoren@gmail.com>
2022-10-11 20:52:43 +0300

    header

--- React/rc4/tour-pl/src/App.js
+++ React/rc4/tour-pl/src/App.js
@@ -1,15 +1,12 @@
 import "./App.scss";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Welcome to react</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat cum
-        animi temporibus. In, reiciendis minus eveniet expedita mollitia aut,
-        consequatur aperiam cupiditate dolore provident, dignissimos quae
-        tempore! Laudantium, dolorum alias.
-      </p>
+      <Navbar />
+      <Header />
     </div>
   );
 };
