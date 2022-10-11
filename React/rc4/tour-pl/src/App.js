09ca1d38dfe907c885b7f95a46c891bea33d4cef
berkay oren <benberkayoren@gmail.com>
2022-10-11 20:34:39 +0300

    start

---
+++ React/rc4/tour-pl/src/App.js
@@ -0,0 +1,17 @@
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to react</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat cum
+        animi temporibus. In, reiciendis minus eveniet expedita mollitia aut,
+        consequatur aperiam cupiditate dolore provident, dignissimos quae
+        tempore! Laudantium, dolorum alias.
+      </p>
+    </div>
+  );
+};
+
+export default App;
