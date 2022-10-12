ea88ac21cb942ef408a8a06671ecfc36dfa8acf6
berkay oren <benberkayoren@gmail.com>
2022-10-12 20:24:03 +0300

    added bootstrap

--- React/rc05_events_useStateHook/src/1-events/Events.jsx
+++ React/rc05_events_useStateHook/src/1-events/Events.jsx
@@ -11,7 +11,11 @@
 //? Ayrinti icin : https://reactjs.org/docs/events.html
 
 const Events = () => {
-  return <div>Events</div>;
+  return (
+    <div className="container text-center mt-4">
+      <button className="btn btn-success">Click</button>
+    </div>
+  );
 };
 
 export default Events;
--- React/rc05_events_useStateHook/src/App.js
+++ React/rc05_events_useStateHook/src/App.js
@@ -1,7 +1,11 @@
+import Events from "./1-events/Events";
+
 function App() {
   return (
     <div>
-      <h1>EVENTS</h1>
+      <h1>
+        <Events />
+      </h1>
     </div>
   );
 }
