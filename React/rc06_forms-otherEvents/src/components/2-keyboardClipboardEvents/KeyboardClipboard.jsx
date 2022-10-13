4e5483f83ce179963eb06028871e555dcdb62a4d
berkay oren <benberkayoren@gmail.com>
2022-10-13 22:39:25 +0300

    oncopy

--- React/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ React/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -11,6 +11,11 @@ const KeyboardClipboard = () => {
     }
     e.keycode === 13 && setInputValue("");
   };
+  const handleAreaPaste = (e) => {
+    e.target.style.border = "2px solid red";
+    e.target.value += e.clipboardData.getData("text").toLocaleUpperCase();
+    e.preventDefault();
+  };
 
   return (
     <div className="container text-center">
@@ -25,7 +30,22 @@ const KeyboardClipboard = () => {
         onKeyDown={handleKeyDown}
       />
 
-      <p className="text-start mt-4">{inputValue}</p>
+      <p
+        onCopy={(e) => {
+          e.preventDefault();
+          alert("you cannot copy this doc");
+        }}
+        className="text-start mt-4"
+      >
+        {inputValue}
+      </p>
+      <textarea
+        className="form-control"
+        name="area"
+        id="area"
+        rows="10"
+        onPaste={handleAreaPaste}
+      ></textarea>
     </div>
   );
 };
