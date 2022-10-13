e8347e8b1a7a3468c330cb41941fe19f5cef1151
berkay oren <benberkayoren@gmail.com>
2022-10-13 20:44:51 +0300

    commit

--- React/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
+++ React/rc06_forms-otherEvents/src/components/1-forms/Form.jsx
@@ -1,41 +1,66 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    // e.preventDefault();
+    console.log(e.target);
+    console.log("Submitted");
+    alert(`username: ${username}
+          email:${email},
+          password:${password}
+    `);
+  };
+
+  const handleUsername = (e) => {
+    console.log(e.target.value);
+    setUsername(e.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1 className="display-5 text-danger">FORMS</h1>
+
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
+        <label htmlFor="username" className="form-label">
+          Username: <span className="text-danger">{username}</span>
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          value={username}
+          onChange={handleUsername}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label">
+          Email: <span className="text-danger">{email}</span>
         </label>
         <input
           type="email"
           className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
-        </div>
       </div>
       <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
+        <label htmlFor="password" className="form-label">
           Password
         </label>
         <input
           type="password"
           className="form-control"
-          id="exampleInputPassword1"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" htmlFor="exampleCheck1">
-          Check me out
-        </label>
-      </div>
+
       <button type="submit" className="btn btn-primary">
         Submit
       </button>
