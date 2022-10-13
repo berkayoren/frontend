1ce0d18f4a4cf4ca39c98778099fad6bf0863994
berkay oren <benberkayoren@gmail.com>
2022-10-13 20:56:23 +0300

    object

--- React/rc06_forms-otherEvents/src/App.js
+++ React/rc06_forms-otherEvents/src/App.js
@@ -1,4 +1,5 @@
 import Form from "./components/1-forms/Form";
+import FormObject from "./components/1-forms/FormObject";
 // import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard";
 // import MouseEvent from "./components/3-mouseEvents/MouseEvent";
 
@@ -8,6 +9,7 @@ function App() {
       <Form />
       {/* <KeyboardEvents />
       <MouseEvent /> */}
+      <FormObject />
     </div>
   );
 }
---
+++ React/rc06_forms-otherEvents/src/components/1-forms/FormObject.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const FormObject = () => {
+  const [formValues, setFormValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const { username, password, email } = formValues;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    console.log("Submitted");
+    alert(`username: ${username}
+          email:${email},
+          password:${password}
+    `);
+  };
+  const handleForm = () => {};
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1 className="display-5 text-danger">FORM OBJECT</h1>
+
+      <div className="mb-3">
+        <label htmlFor="username" className="form-label">
+          Username: <span className="text-danger">{username}</span>
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          value={formValues.username}
+          onChange={handleForm}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label">
+          Email: <span className="text-danger">{email}</span>
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          value={email}
+          onChange={handleForm}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="password" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          value={password}
+          onChange={handleForm}
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default FormObject;
