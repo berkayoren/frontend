f8b13a7e6ee50453fe5ab1649afd42d7534fc1fb
berkay oren <benberkayoren@gmail.com>
2022-10-17 22:44:06 +0300

    user

---
+++ React/rc08_lifecycle-useEffect/src/components/User.jsx
@@ -0,0 +1,12 @@
+const User = () => {
+  return (
+    <div>
+      <h1>Name</h1>
+      <img src="" alt="" />
+      <p>Email</p>
+      <h5>Day of Birth</h5>
+    </div>
+  );
+};
+
+export default User;
