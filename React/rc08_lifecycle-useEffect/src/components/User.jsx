4619f9d79f16c864338348c4f8c3d823f366d9e2
berkay oren <benberkayoren@gmail.com>
2022-10-17 22:57:29 +0300

    commit

--- React/rc08_lifecycle-useEffect/src/components/User.jsx
+++ React/rc08_lifecycle-useEffect/src/components/User.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
+
 const User = () => {
+  const [user, setUser] = useState("");
+
+  const getUser = () => {
+    fetch("https://randomuser.me/api")
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
   return (
     <div>
       <h1>Name</h1>
       <img src="" alt="" />
       <p>Email</p>
       <h5>Day of Birth</h5>
+      <button className="btn btn-danger" onClick={getUser}>
+        Get User
+      </button>
     </div>
   );
 };
