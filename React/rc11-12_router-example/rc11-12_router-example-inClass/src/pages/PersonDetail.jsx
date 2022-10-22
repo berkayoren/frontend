d9d9ded8dfa7d736cda6a03691ea2c811d2f1157
berkay oren <benberkayoren@gmail.com>
2022-10-22 13:57:18 +0300

    a

--- React/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ React/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -1,5 +1,30 @@
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+
 const PersonDetail = () => {
-  return <div>PersonDetail</div>;
+  const navigate = useNavigate();
+  const { id } = useParams();
+  console.log(id);
+
+  const { state: person } = useLocation();
+  console.log(person);
+  return (
+    <div className="container text-center">
+      <h3>
+        {" "}
+        {person.first_name} {person.last_name}
+      </h3>
+      <img className="rounded-circle" src={person.avatar} alt="" />
+      <p> {person.email} </p>
+      <div>
+        <button onClick={() => navigate("/")} className="btn btn-success me-2">
+          HOME
+        </button>
+        <button onClick={() => navigate(-1)} className="btn btn-warning">
+          BACK
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PersonDetail;
