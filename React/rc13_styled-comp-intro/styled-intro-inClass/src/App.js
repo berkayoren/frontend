b1238a899bc5f209e51faa7969970ef335fcc7fe
berkay oren <benberkayoren@gmail.com>
2022-10-25 21:25:47 +0300

    link

--- React/rc13_styled-comp-intro/styled-intro-inClass/src/App.js
+++ React/rc13_styled-comp-intro/styled-intro-inClass/src/App.js
@@ -1,16 +1,20 @@
 import { Button, TomatoButton } from "./components/Button.styled";
 import Container from "./components/Container.styled";
 import HeaderText from "./components/HeaderText.styled";
+import StyledLink from "./components/Link.styled";
 
 const App = () => {
   return (
     <>
-      <Container bg="green">
+      <Container bg="grey">
         <HeaderText color="hotpink">STYLED COMPONENTS</HeaderText>
         <Button primary> Save</Button>
         <Button>Clear</Button>
         <TomatoButton>Send</TomatoButton>
         <TomatoButton primary>Submit</TomatoButton>
+      </Container>
+      <Container>
+        <StyledLink href="https://google.com" target="blank"></StyledLink>
       </Container>
     </>
   );
