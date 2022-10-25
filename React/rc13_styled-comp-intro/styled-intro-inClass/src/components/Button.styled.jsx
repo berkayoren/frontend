87f2f32f87693f4bd30bad42e60b63b22f5dddcd
berkay oren <benberkayoren@gmail.com>
2022-10-25 20:35:48 +0300

    commit

--- React/rc13_styled-comp-intro/styled-intro-inClass/src/components/Button.styled.jsx
+++ React/rc13_styled-comp-intro/styled-intro-inClass/src/components/Button.styled.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import styled from "styled-components";
 
-const Button.styled = () => {
-  return (
-    <div>Button.styled</div>
-  )
-}
-
-export default Button.styled
+export const Button = styled.button`
+  background-color: hotpink;
+  color: white;
+  cursor: pointer;
+  padding: 1rem 2rem;
+  border-radius: 0.5rem;
+  border-color: pink;
+  &:hover {
+    transform: scale(0.97);
+  }
+`;
