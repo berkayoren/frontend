2d135df930d215c897ea4d02ec6c71f20fe263d0
berkay oren <benberkayoren@gmail.com>
2022-10-25 20:52:05 +0300

    tomato

--- React/rc13_styled-comp-intro/styled-intro-inClass/src/App.js
+++ React/rc13_styled-comp-intro/styled-intro-inClass/src/App.js
@@ -1,10 +1,12 @@
-import { Button } from "./components/Button.styled";
+import { Button, TomatoButton } from "./components/Button.styled";
 
 const App = () => {
   return (
     <>
       <Button primary> Save</Button>
       <Button>Clear</Button>
+      <TomatoButton>Send</TomatoButton>
+      <TomatoButton primary>Submit</TomatoButton>
     </>
   );
 };
