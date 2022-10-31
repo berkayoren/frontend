89536475c2acf98f4616acf61c71445d60c11562
berkay oren <benberkayoren@gmail.com>
2022-10-31 22:55:02 +0300

    a

--- React/rc14-recipe-app/recipe-app-starter/src/components/nav/Navbar.jsx
+++ React/rc14-recipe-app/recipe-app-starter/src/components/nav/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
 import Nav, { Brand, Menu, MenuLink, Hamburger } from "./Navbar.style";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,11 +14,12 @@ const Navbar = () => {
         <i>{"<Clarusway/>"}</i>
         <span>Recipe</span>
       </Brand>
+
       <Hamburger onClick={() => setIsOpen(!isOpen)}>
         <GiHamburgerMenu />
       </Hamburger>
 
-      <Menu>
+      <Menu isOpen={isOpen} onClick={() => setIsOpen(false)}>
         <MenuLink to="/">Home</MenuLink>
         <MenuLink to="about">About</MenuLink>
         <MenuLink to="register">Register</MenuLink>
