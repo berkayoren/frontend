9c833620f9fe7a0dcc6996c54490d6e00002847e
berkay oren <benberkayoren@gmail.com>
2022-11-01 20:33:49 +0300

    a

--- React/rc14-recipe-app/recipe-app-starter/src/components/header/Header.jsx
+++ React/rc14-recipe-app/recipe-app-starter/src/components/header/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./Header.style";
+import { HeaderContainer } from "./Header.style";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <HeaderContainer>
+      <MainHeader>Recipe App</MainHeader>
+      <FormContainer>
+        <FoodInput type="text" placeholder="Search"></FoodInput>
+      </FormContainer>
+    </HeaderContainer>
+  );
+};
 
-export default Header
+export default Header;
--- React/rc14-recipe-app/recipe-app-starter/src/pages/home/Home.jsx
+++ React/rc14-recipe-app/recipe-app-starter/src/pages/home/Home.jsx
@@ -2,16 +2,21 @@ import axios from "axios";
 import React from "react";
 import { useEffect, useState } from "react";
 
+const Home = () => {};
+
 const APP_ID = "4e9f05eb";
 const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
-const [query, setQuery] = useState();
+const [query, setQuery] = useState("egg");
 const [selectedMeal, setSelectedMeal] = useState("breakfast");
+const [recipes, setRecipes] = useState("");
 
 const Home = () => {
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
 
   const getData = async () => {
-    const { dsata } = await axios(url);
+    const { data } = await axios(url);
+
+    setRecipes(data.hits);
   };
   useEffect(() => {
     getData();
