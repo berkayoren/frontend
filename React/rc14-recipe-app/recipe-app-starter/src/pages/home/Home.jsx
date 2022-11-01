c111668836660b40dd3e09f828e28fd34829253c
berkay oren <benberkayoren@gmail.com>
2022-11-01 20:21:45 +0300

    a

--- React/rc14-recipe-app/recipe-app-starter/src/pages/home/Home.jsx
+++ React/rc14-recipe-app/recipe-app-starter/src/pages/home/Home.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
+
+const APP_ID = "4e9f05eb";
+const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
+const [query, setQuery] = useState();
+const [selectedMeal, setSelectedMeal] = useState("breakfast");
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
 
-export default Home
+  const getData = async () => {
+    const { dsata } = await axios(url);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <div>Home</div>;
+};
+
+export default Home;
