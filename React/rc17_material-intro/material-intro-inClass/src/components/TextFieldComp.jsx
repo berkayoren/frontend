97df1de44e05a43d80219a36da427011251ac69c
berkay oren <benberkayoren@gmail.com>
2022-11-03 21:48:47 +0300

    a

--- React/rc17_material-intro/material-intro-inClass/src/components/TextFieldComp.jsx
+++ React/rc17_material-intro/material-intro-inClass/src/components/TextFieldComp.jsx
@@ -1,17 +1,44 @@
+import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import Box from "@mui/material/Box";
 
 const TextFieldComp = () => {
+  const [error, setError] = useState(false);
+
   return (
     <div>
       <Container>
-        <Typography variant="h4" color="error">
+        <Typography variant="h4" color="error" align="center" mt={4}>
           TEXT FIELD
         </Typography>
 
-        <Box>
-          <TextField />
+        <Box sx={{ mt: 4, textAlign: "center" }}>
+          <TextField
+            margin="dense"
+            id="email"
+            label="Email"
+            placeholder="Enter your email"
+            fullWidth
+            error={error}
+            helperText={error && "Incorrect email format"}
+          />
+        </Box>
+        <Box sx={{ mt: 4 }}>
+          <TextField
+            margin="normal"
+            id="password"
+            label="Password"
+            placeholder="Enter your passWord"
+            fullWidth
+            error={error}
+            helperText={error && "Incorrect password"}
+          />
+          <Button variant="contained" color="warning" sx={{ mt: 4 }}>
+            Submit
+          </Button>
         </Box>
       </Container>
     </div>
