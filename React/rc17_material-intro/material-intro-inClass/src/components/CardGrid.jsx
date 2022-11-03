104aac8184b50e46f0c6c4ed799535b27523cc68
berkay oren <benberkayoren@gmail.com>
2022-11-03 21:58:48 +0300

    grid

--- React/rc17_material-intro/material-intro-inClass/src/components/CardGrid.jsx
+++ React/rc17_material-intro/material-intro-inClass/src/components/CardGrid.jsx
@@ -1,43 +1,45 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import { Button, CardActionArea, CardActions } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import Grid from "@mui/material/Grid";
 import data from "../data";
 
-export default function MediaCard() {
+export default function CardGrid() {
   return (
     <>
-      {data.map((card) => {
-        const { id, text, img, name } = card;
-        return (
-          <div>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardMedia
-                component="img"
-                height="140"
-                image="/static/images/cards/contemplative-reptile.jpg"
-                alt="img"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {text}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
-          </div>
-        );
-      })}
+      <Typography variant="h4" color="error" align="center" mt={4}>
+        CARD and GRID
+      </Typography>
+      <Grid container spacing={2} justifyContent="center" alignItems="center">
+        {data.map((card) => {
+          const { id, text, img, name } = card;
+          return (
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia component="img" image={img} alt="img" />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {text}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Share
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
     </>
   );
 }
