7bbb3e2f8ec1c407f8528c808b8a765689fd0405
berkay oren <benberkayoren@gmail.com>
2022-11-08 22:26:03 +0300

    a

--- React/rc18-19_firebase-movie-app/firebase-movie-app-solution/src/pages/Main.jsx
+++ React/rc18-19_firebase-movie-app/firebase-movie-app-solution/src/pages/Main.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
-import MovieDetail from "./MovieDetail";
 import MovieCard from "../components/MovieCard";
+import { AuthContext } from "../context/AuthContextProvider";
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
@@ -9,24 +9,63 @@ const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
     getMovies(FEATURED_API);
   }, []);
 
   const getMovies = (API) => {
+    setLoading(true);
     axios
       .get(API)
       .then((res) => setMovies(res.data.results))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getMovies(SEARCH_API + searchTerm);
+    setSearchTerm("");
+    if (searchTerm && currentUser) {
+      setSearchTerm("");
+    } else if (currentUser) {
+      alert("Please Login to See Details.");
+    } else {
+      alert("Please Enter a Text.");
+    }
   };
 
   return (
-    <div className="flex justify-center flex-wrap">
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} {...movie} />
-      ))}
-    </div>
+    <>
+      <form className="flex justify-center p-2" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="w-80 h-8 rounded-md outline-none border p-1 m-2"
+          placeholder="Search a movie..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
+        />
+        <button className="text-white" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="flex justify-center flex-wrap">
+        {loading ? (
+          <div
+            className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600 mt-52"
+            role="status"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        ) : (
+          movies.map((movie) => <MovieCard key={movie.id} {...movie} />)
+        )}
+      </div>
+    </>
   );
 };
 
