bb761e256d8b69cd3f4b8b6032369e2f8bae8451
berkay oren <benberkayoren@gmail.com>
2022-11-08 22:52:23 +0300

    a

--- React/rc18-19_firebase-movie-app/firebase-movie-app-solution/src/components/MovieCard.jsx
+++ React/rc18-19_firebase-movie-app/firebase-movie-app-solution/src/components/MovieCard.jsx
@@ -20,7 +20,13 @@ const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
     }
   };
   return (
-    <div className="movie" onClick={() => navigate("details/" + id)}>
+    <div
+      className="movie"
+      onClick={() => {
+        navigate("details/" + id);
+        !currentUser && alert("Please Login to See Details.");
+      }}
+    >
       <img
         loading="lazy"
         src={poster_path ? IMG_API + poster_path : defaultImage}
--- React/rc18-19_firebase-movie-app/firebase-movie-app-solution/src/pages/MovieDetail.jsx
+++ React/rc18-19_firebase-movie-app/firebase-movie-app-solution/src/pages/MovieDetail.jsx
@@ -1,19 +1,11 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import React from "react";
 
 const MovieDetail = () => {
-  const [movieDetails, setMovieDetails] = useState("");
-  const { id } = useParams();
   const API_KEY = process.env.REACT_APP_TMDB_KEY;
   const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
   const baseImageUrl = "https://image.tmdb.org/t/p/w1280";
   const defaultImage =
     "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
-
-  useEffect(() => {
-    axios.get(movieDetailBaseUrl).then((res) => console.log(res.data));
-  }, []);
 
   return (
     <div className="container px-10 mx-auto py-5">
