b9cfa58c08b3e2085891096894d66c2d2b8f37ca
berkay oren <benberkayoren@gmail.com>
2022-11-10 21:29:50 +0300

    provider

--- React/rc21_redux-intro/redux-intro-inClass/src/App.js
+++ React/rc21_redux-intro/redux-intro-inClass/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Counter from "./components/counter/Counter";
 import Todo from "./components/todo/Todo";
+import { legacy_createStore as createStore } from "redux";
+import reducer from "./redux";
+import { Provider } from "react";
 
 function App() {
+  const store = createStore(reducer);
   return (
     <div className="app">
-      <Counter />
+      <Provider store={store}>
+        <Counter />
+      </Provider>
       {/* <Todo /> */}
     </div>
   );
