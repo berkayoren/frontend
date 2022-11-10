f7cf47927e6a764192c698042e93cf987c535b84
berkay oren <benberkayoren@gmail.com>
2022-11-10 20:12:47 +0300

    start

--- React/rc20_useref-usememo-usecallback/useref-memo-callback-inclass/src/components/HeaderMemo.jsx
+++ React/rc20_useref-usememo-usecallback/useref-memo-callback-inclass/src/components/HeaderMemo.jsx
@@ -1,8 +1,10 @@
 import React, { memo } from "react";
 
-const HeaderMemo = () => {
+const HeaderMemo = ({ count }) => {
   console.log("Render => HeaderMemo Components");
-  return <div className="bg-danger text-center">HeaderMemo Componenti</div>;
+  return (
+    <div className="bg-danger text-center">HeaderMemo Componenti : {count}</div>
+  );
 };
 
 export default memo(HeaderMemo);
