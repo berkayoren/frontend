65af5d44f2f5f49bd7d40252fae8c26379d7cb3f
berkay oren <benberkayoren@gmail.com>
2022-11-14 23:04:05 +0300

    last

--- React/rc23_Fire-contact/fire_contact_project_solution/src/App.js
+++ React/rc23_Fire-contact/fire_contact_project_solution/src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import Contacts from "./components/contacts/Contacts";
 import FormComponent from "./components/form/FormComponent";
 import { useState } from "react";
-import { AddUser } from "./utils/functions";
+import { AddUser, UpdateUser } from "./utils/functions";
+import { ToastContainer } from "react-toastify";
 
 const initialValues = {
   username: "",
@@ -14,7 +15,16 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    AddUser(info);
+    if (info.id) {
+      UpdateUser(info);
+    } else {
+      AddUser(info);
+    }
+    setInfo(initialValues);
+  };
+
+  const editUser = (id, username, phoneNumber, gender) => {
+    setInfo({ id, username, phoneNumber, gender });
   };
 
   return (
@@ -24,7 +34,8 @@ function App() {
         setInfo={setInfo}
         handleSubmit={handleSubmit}
       />
-      <Contacts />
+      <Contacts editUser={editUser} />
+      <ToastContainer />
     </div>
   );
 }
