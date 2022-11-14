2d1a027834e8b0b402f8d474c69d6d1c9dfaa73d
berkay oren <benberkayoren@gmail.com>
2022-11-14 21:01:19 +0300

    a

---
+++ React/rc23_Fire-contact/fire_contact_project_solution/src/utils/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDN8b175vcLliIZI3_LjIoEew3EAIoVlNo",
+  authDomain: "fire-contact-project-61e8c.firebaseapp.com",
+  databaseURL: "https://fire-contact-project-61e8c-default-rtdb.firebaseio.com",
+  projectId: "fire-contact-project-61e8c",
+  storageBucket: "fire-contact-project-61e8c.appspot.com",
+  messagingSenderId: "417944412102",
+  appId: "1:417944412102:web:964b5f640e5542f2955cc7",
+};
+
+const app = initializeApp(firebaseConfig);
