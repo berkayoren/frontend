a5f5927c962b261d1eeee0e1bb40c0d658adb613
berkay oren <benberkayoren@gmail.com>
2022-11-14 22:18:54 +0300

    a

--- React/rc23_Fire-contact/fire_contact_project_solution/src/utils/function.js
+++ React/rc23_Fire-contact/fire_contact_project_solution/src/utils/function.js
@@ -1,6 +1,8 @@
 // ADD USER
-import { getDatabase, ref, set, push } from "firebase/database";
+
+import { getDatabase, ref, set, push, onValue } from "firebase/database";
 import firebase from "./firebase";
+import { useState, useEffect } from "react";
 
 export const AddUser = (info) => {
   const db = getDatabase(firebase);
@@ -12,5 +14,26 @@ export const AddUser = (info) => {
     phoneNumber: info.phoneNumber,
     gender: info.gender,
   });
-  console.log("Add userda bilgiler kaydedildi");
+};
+
+// READ INFO
+export const useFetch = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactList, setContactList] = useState();
+  useEffect(() => {
+    const db = getDatabase(firebase);
+    const userRef = ref(db, "user/");
+
+    onValue(userRef, (snapshot) => {
+      const data = snapshot.val();
+      const userArray = [];
+
+      for (let id in data) {
+        userArray.push({ id, ...data[id] });
+      }
+      setContactList(userArray);
+      setIsLoading(false);
+    });
+  }, []);
+  return { isLoading, contactList };
 };
