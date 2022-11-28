b037645c894c01e0d489e8d650bd8dcb3e0154c6
berkay oren <benberkayoren@gmail.com>
2022-11-28 21:26:08 +0300

    ts

--- JavaScript/ts01-intro-basics/src/index.ts
+++ JavaScript/ts01-intro-basics/src/index.ts
@@ -1,5 +1,9 @@
 export {};
 console.log("Hello TypeScript!");
-let a = 5;
-let b = "hello";
-console.log(a);
+
+let myVar: any = "cw";
+myVar = 77;
+
+let numbers: number[] = []; // = [1, 2, 3];
+
+numbers.push("3");
